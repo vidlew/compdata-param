5140a471adddc3fb7af977a3ddff65eb39280787
vidlew <vidlew@outllook.com>
2022-12-05 10:26:04 -0800

    new file:   src/Data/Comp/Param/Multi/HDifoldable.hs

---
+++ src/Data/Comp/Param/Multi/HDifoldable.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE Rank2Types, FlexibleInstances, MultiParamTypeClasses,
+  FlexibleContexts, TypeOperators, GADTs #-}
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  Data.Comp.Param.Multi.HDitraversable
+-- Copyright   :  (c) 2011 Patrick Bahr, Tom Hvitved
+-- License     :  BSD3
+-- Maintainer  :  Tom Hvitved <hvitved@diku.dk>
+-- Stability   :  experimental
+-- Portability :  non-portable (GHC Extensions)
+--
+-- This module defines traversable higher-order difunctors.
+--
+--------------------------------------------------------------------------------
+
+module Data.Comp.Param.Multi.HDifoldable
+    (
+     HDifoldable (..),
+     HFoldable (..)
+    ) where
+
+import Prelude hiding (mapM, sequence, foldr)
+import Data.Comp.Multi.HFunctor
+import Data.Comp.Multi.HFoldable
+import Data.Comp.Param.Multi.HDifunctor
+import Data.Comp.Param.Multi.Algebra
+import Data.Coerce
+import Data.Monoid
+
+instance Semigroup m => Semigroup (K m i) where
+    (K a) <> (K b) = K $ a<>b
+
+instance Monoid m => Monoid (K m i) where
+    mempty = K mempty
+
+{-| HDifunctors representing data structures that can be folded. -}
+class HDifunctor f => HDifoldable f where
+    hdifold :: Monoid m => Alg f (K m)
+    hdifold = hdifoldMap coerce
+
+    hdifoldMap :: Monoid m => (a :=> m) -> f a a :=> m
+    hdifoldMap f = hdifoldr (mappend . f) mempty
+    
+    hdifoldr :: (a :=> (b -> b)) -> b -> f a a :=> b
+    hdifoldr f z t = appEndo (hdifoldMap (Endo . f) t) z
+
+    hdifoldl :: (b -> (a :=> b)) -> b -> f a a :=> b
+    hdifoldl f z t = appEndo (getDual (hdifoldMap (Dual . Endo . flip f) t)) z
